97c427d6234a64e182e06f8b9cbe6533896d589e
petrikov.andrey <petrikov.andrey@yandex.ru>
2022-04-15 10:31:07 +0300

    task1

---
+++ react1/src/components/ShopItemFunc.js
@@ -0,0 +1,21 @@
+function ShopItemFunc(props) {
+  const  {brand,title,description,descriptionFull,currency,price} = props.item;
+
+  return (
+    <div className="main-content">
+      <h2>{brand}</h2>
+      <h1>{title}</h1>
+      <h3>{description}</h3>
+      <div className="description">{descriptionFull}</div>
+      <div className="highlight-window mobile">
+        <div className="highlight-overlay"></div>
+      </div>
+      <div className="divider"></div>
+      <div className="purchase-info">
+        <div className="price">{currency}{price}</div>
+        <button>Добавить в корзину</button>
+      </div>
+    </div>
+  );
+}
+export default ShopItemFunc;
